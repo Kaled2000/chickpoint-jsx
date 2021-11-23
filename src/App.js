ba6a6b36b5bfd8573b3e2a9684ea427224c21c30
Khalid2000 <kaled20032000@yahoo.com>
2021-11-23 19:36:42 +0300

    Adding Project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import kh from './kh.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">KHALID</h1>
+          <br />
+              </div>
+
+          <img src="/1.jpg"  />
+          <br />
+          <img src={kh} alt="logo" />
+          <br />
+          <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+    </div>
+  );
+}
+
+export default App;
